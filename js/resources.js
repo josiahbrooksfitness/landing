28bf4387396ad3bfe6fe083ec367ac3aed4b47a7
Adam Webster <adam.webster@live.com>
2024-10-27 23:14:14 -0400

    Refactoring JS to prepare for customizations.

---
+++ js/resources.js
@@ -0,0 +1,90 @@
+var resources = {
+    brandname: "Josiah Brooks Fitness",
+    contact: {
+        phone: "+16074184560",
+        email: "josiahbrooksfitness@gmail.com"
+    },
+
+    mastheadSubline: "Training & Nutrition Programs Designed For YOU",
+
+    callToAction: "Ready to get started? Call, Text, or Email Now!",
+
+    missionStatement: `My mission is to empower individuals to achieve lasting health by addressing not
+only their physical fitness but also their mental and emotional well-being. I believe
+that a healthy body is built on the foundation of a healthy mind, and I am
+committed to helping my clients overcome roadblocks such as stress and negative
+self-talk. By incorporating mental exercises, establishing sustainable habits, and
+creating personalized action plans, I strive to provide the tools necessary for long-
+term success, enabling my clients to lead healthier, more balanced lives`,
+
+    bio: `My fitness journey began early, driven by my father’s influence and a passion for
+working out. In 2013, I committed to a healthier lifestyle, losing 40 pounds in 18
+months. Down the road I realized the importance of balancing physical fitness with
+mental well-being. Unhealthy habits and unstable emotional health led to burnout
+and eventually contributed to a herniated disc injury. A two-year recovery period
+tested my determination, but in 2023, I began my comeback, and now, in 2024, I’m
+thriving once again. Through my personal experience with burnout, injury, and
+recovery, I’m passionate about helping others achieve their goals and overcome
+obstacles on their own fitness journeys.`,
+
+    clientExpectation: `As a personal trainer, I create a welcoming and supportive environment to help
+ease any anxiety about starting a new routine. Whether you’re new to fitness or
+have experience, I meet you where you are and guide you from there.`,
+
+    myExpectation: `From my clients, I expect honesty, integrity, and transparency. I need an accurate
+understanding of where you’re starting from to effectively guide you toward your
+goals. By being open about your habits, progress, and challenges, we can work
+together to make the necessary adjustments and ensure your success. My goal is
+to create a collaborative and supportive relationship where both of us are
+committed to achieving the best possible outcomes for your health and fitness
+journey.`,
+
+    modalities: [
+        {
+            title: "Longevity/Mobility - All Ages (Teens 14+)",
+            description: `This program is designed for anyone looking to maintain long-term physical health.
+The focus is on maintaining muscle mass and addressing any existing muscle
+imbalances that may limit mobility or cause discomfort. By enhancing hand-eye
+coordination, we also promote improved daily functional movements. Additionally,
+strengthening brain activity through physical exercise helps in maintaining
+cognitive health, benefiting individuals of all ages, from teens to seniors.`
+        },
+        {
+            title: "Summer Shape-Up - Adults Ages 21-50",
+            description: `A dynamic, results-driven program tailored to adults who want to enhance their
+physique and stamina for the summer months. This regimen targets the
+midsection, helping to tone and define the abdominal muscles. We also work to
+strengthen and firm the legs and glutes, building lean muscle throughout the
+upper body. Whether you’re looking to boost your endurance, improve overall
+body tone, or add lean muscle mass, this program will help you achieve that
+summer-ready look while improving your overall fitness.`
+        },
+        {
+            title: "Strength Training - Adults Ages 18-50",
+            description: `Aimed at those who want to build strength and power, this program focuses on
+progressive loading to continually challenge the muscles and promote growth. The
+routine emphasizes proper form to maximize gains and prevent injury, while also
+incorporating phases of de-loading to allow the body to recover. This training is
+complemented by comprehensive nutrition coaching, including guidance on
+macronutrients to fuel your body for optimal performance and muscle building.`
+        }
+    ],
+
+    trainingComponents: [
+        "Body Weight and Measurement",
+        "Vision Centered Guidance",
+        "Creating Action Plans",
+        "Establishing Healthier Habits",
+        "Stress Reduction",
+        "Bloodwork Monitoring",
+        "Blood Pressure Monitoring"
+    ],
+
+    socialLinks: {
+        facebook: "#!",
+        x: "#!",
+        instagram: "#!",
+    },
+
+    now: new Date()
+};
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,135 @@
+class LandingPage {
+    constructor() {}
+
+    contactButtons = `<div class="row justify-content-center contact-links">
+        <div class="col-lg-4">
+            <a href="tel:${resources.contact.phone}"><i class="bi bi-telephone fs-3 text-white"></i></a>
+        </div>
+        <div class="col-lg-4">
+            <a href="sms:${resources.contact.phone}"><i class="bi bi-chat-left-dots fs-3 text-white"></i></a>
+        </div>
+        <div class="col-lg-4">
+            <a href="mailto:${resources.contact.email}"><i class="bi bi-envelope fs-3 text-white"></i></a>
+        </div>
+    </div>`
+
+    masthead = `<header class="masthead">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+                        <div class="text-center text-white">
+                            <h1 class="mb-5 bonheur-royale-regular" id="main-head">${resources.brandname}</h1>
+                            <h1 class="mb-5">${resources.mastheadSubline}</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="row feature-icons text-center">
+                    <div class="col-lg-4">${this.getIcon("bi-egg-fried", "Nutrition", "Fulfill Your Body's Needs!")}</div>
+                    <div class="col-lg-4">${this.getIcon("bi-graph-up-arrow", "Strength", "Grow Your Confidence!")}</div>
+                    <div class="col-lg-4">${this.getIcon("bi-heart-pulse", "Cardio", "Build All Day Energy!")}</div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-6">
+                        <div class="text-center text-white">${this.contactButtons}</div>
+                    </div>
+                </div>
+            </div>
+        </header>`;
+
+    imageShowcase = `<section class="showcase">
+        <div class="container-fluid p-0">
+            <div class="row g-0">${this.getShowcaseItem("assets/img/bg-showcase-1.jpg", "A Solid Foundation", "For your body to be primed and ready for dramatic transformation, it must be built on a fundamentally sound diet. Level up with tailored nutrition plans designed to fit your personal needs and goals!", true)}</div>
+            <div class="row g-0">${this.getShowcaseItem("assets/img/bg-showcase-2.jpg", "Strength Training", "Whether you dream of having a physique worthy of a competition stage or you simply long to hold a grandchild, personalized strength training plans will take you from moving pebbles to moving mountains!", false)}</div>
+            <div class="row g-0">${this.getShowcaseItem("assets/img/bg-showcase-3.jpg", "Cardio", "Lacking energy as the day wears on? Stairs stealing your breath more than you'd like to admit? Optimized cardio sessions will give your mental and physical stamina a jump start!", true)}</div>
+        </div>
+    </section>`;
+
+    testimonials = `<section class="testimonials text-center bg-light">
+            <div class="container">
+                <h2 class="mb-5">What people are saying...</h2>
+                <div class="row">
+                    <div class="col-lg-4">${this.getTestimonialItem("assets/img/testimonials-1.jpg", "Johnathan E.", "This is fantastic! Bulking programs have never been so easy!")}</div>
+                    <div class="col-lg-4">${this.getTestimonialItem("assets/img/testimonials-2.jpg", "Jessica S.", "Regular cardio sessions give me the long lasting energy to go anywhere and do anything!")}</div>
+                    <div class="col-lg-4">${this.getTestimonialItem("assets/img/testimonials-3.jpg", "William H.", "Having a complete fitness approach has allowed me to achieve my goals at the right pace for me!")}</div>
+                </div>
+            </div>
+        </section>`;
+
+    callToAction = `<section class="call-to-action text-white text-center" id="signup">
+        <div class="container position-relative">
+            <div class="row justify-content-center">
+                <div class="col-xl-6">
+                    <h2 class="mb-4">${resources.callToAction}</h2>
+                    ${this.contactButtons}
+                </div>
+            </div>
+        </div>
+    </section>`;
+
+    footer = `<footer class="footer bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
+                        <p class="text-muted small mb-4 mb-lg-0">&copy; ${resources.brandname} ${resources.now.getFullYear()}. All Rights Reserved.</p>
+                    </div>
+                    <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item me-4">
+                                <a href="${resources.socialLinks.facebook}"><i class="bi-facebook fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item me-4">
+                                <a href="${resources.socialLinks.x}"><i class="bi-twitter-x fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="${resources.socialLinks.instagram}"><i class="bi-instagram fs-3"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>`;
+
+    getIcon(icon, title, description) {
+        return `<div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                <div class="features-icons-icon d-flex"><i class="${icon} m-auto text-primary"></i></div>
+                <h3>${title}</h3>
+                <!-- p class="lead mb-0">${description}</p -->
+            </div>`;
+    }
+
+    getShowcaseItem(imagePath, title, description, orientation) {
+        return `<div class="col-lg-6 ${orientation === true ? 'order-lg-2' : ''} text-white showcase-img" style="background-image: url('${imagePath}')"></div>
+                <div class="col-lg-6 ${orientation === true ? 'order-lg-1' : ''} my-auto showcase-text">
+                    <h2>${title}</h2>
+                    <p class="lead mb-0">${description}</p>
+                </div>`;
+    }
+
+    getTestimonialItem(imagePath, name, testimony) {
+        return `<div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                    <img class="img-fluid rounded-circle mb-3" src="${imagePath}" alt="..." />
+                    <h5>${name}</h5>
+                    <p class="font-weight-light mb-0">"${testimony}"</p>
+                </div>`;
+    }
+
+    toString() {
+        return this.masthead + 
+            this.imageShowcase + 
+            this.testimonials + 
+            this.callToAction + 
+            this.footer;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // set tab title
+    document.getElementsByTagName('title')[0].innerText = resources.brandname;
+
+    var page = new LandingPage();
+    // set body content
+    document.body.innerHTML += page.toString();
+
+    document.body.classList.remove('loading');
+});
 
